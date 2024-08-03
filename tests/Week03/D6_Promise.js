7b445dc6636549946c553983de79b0c1662794b0
ramyasekar207 <75845289+ramyasekar207@users.noreply.github.com>
2024-08-03 14:16:54 +0530

    Update D6_Promise.js

--- tests/Week03/D6_Promise.js
+++ tests/Week03/D6_Promise.js
@@ -1,20 +1,33 @@
 
+let conditionalPromise = new Promise((resolve, reject) => {
+    let random = Math.random();
+    if(random > 0.5){
+        resolve('Resolved successfully. ' + random)
+    }else{
+        reject('Rejected ' + random)
+    }
 
-let Promise = new Promise(function(resolve, reject) {
-    let num =0;
-    if(num>1)
-        {
-            resolve("Resolved successfully");
-        }
-        else
-        reject("Rejected")
-});
 
-function conditionalPromise() {
-}
+}).then((results) => {
+    console.log(results);
+}).catch((error) => {
+    console.log(error);
+})
+// let Promise = new Promise(function(resolve, reject) {
+//     let num =0;
+//     if(num>1)
+//         {
+//             resolve("Resolved successfully");
+//         }
+//         else
+//         reject("Rejected")
+// });
+
+// function conditionalPromise() {
+// }
    
-Promise.then(
-function() {conditionalPromise();},
-function() {conditionalPromise();}
-);
+// Promise.then(
+// function() {conditionalPromise();},
+// function() {conditionalPromise();}
+// );
 
